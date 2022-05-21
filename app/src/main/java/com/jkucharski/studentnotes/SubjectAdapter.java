29d33bc813f607dcdca3b7b9a7d441b0077fd679
root <vixier61@gmail.com>
2022-05-21 19:08:24 +0200

    Implemented background image to subjects and now expand button shows description.

--- app/src/main/java/com/jkucharski/studentnotes/SubjectAdapter.java
+++ app/src/main/java/com/jkucharski/studentnotes/SubjectAdapter.java
@@ -1,25 +1,26 @@
 package com.jkucharski.studentnotes;
 
-import android.content.Context;
-import android.util.AttributeSet;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectVH> {
 
-    static List<SubjectDC> subjectNames = new ArrayList<>();
+    static List<SubjectDC> subjectDC = new ArrayList<>();
 
     @NonNull
     @Override
@@ -31,29 +32,59 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectV
 
     @Override
     public void onBindViewHolder(@NonNull SubjectVH holder, int position) {
-        SubjectDC subjectName = subjectNames.get(position);
-        holder.subjectNameTV.setText(subjectName.name);
+        holder.subjectNameTV.setText(subjectDC.get(position).name);
+        holder.subjectDescTV.setText(subjectDC.get(position).description);
+
+        holder.expandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.hiddenSubjectView.getVisibility() == View.VISIBLE) {
+                    TransitionManager.beginDelayedTransition(holder.cardView,
+                            new AutoTransition());
+                    holder.hiddenSubjectView.setVisibility(View.GONE);
+                    holder.expandButton.setImageResource(R.drawable.ic_baseline_expand_more_24);
+                }
+
+                else {
+
+                    TransitionManager.beginDelayedTransition(holder.cardView,
+                            new AutoTransition());
+                    holder.hiddenSubjectView.setVisibility(View.VISIBLE);
+                    holder.expandButton.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        if(subjectNames == null)
+        if(subjectDC == null)
             return 0;
-        return subjectNames.size();
+        return subjectDC.size();
     }
 
     public class SubjectVH extends RecyclerView.ViewHolder{
-        String subjectName;
         TextView subjectNameTV;
+        TextView subjectDescTV;
+        ImageButton expandButton;
+        LinearLayout hiddenSubjectView;
+        CardView cardView;
+        ImageView subjectBackground;
 
         public SubjectVH(@NonNull View itemView) {
             super(itemView);
             subjectNameTV = itemView.findViewById(R.id.subjectNameTV);
-
+            subjectDescTV = itemView.findViewById(R.id.subjectDescriptionTV);
+            expandButton = itemView.findViewById(R.id.expandButton);
+            hiddenSubjectView = itemView.findViewById(R.id.hiddenSubjectView);
+            cardView = itemView.findViewById(R.id.cardView);
+            subjectBackground = itemView.findViewById(R.id.subjectImageBG);
         }
     }
     public void addSubject(String name, String description){
-        subjectNames.add(new SubjectDC(name, description));
-        notifyItemInserted(subjectNames.size()-1);
+        subjectDC.add(new SubjectDC(name, description));
+        notifyItemInserted(subjectDC.size()-1);
     }
+
+
 }
--- app/src/main/java/com/jkucharski/studentnotes/SubjectDC.java
+++ app/src/main/java/com/jkucharski/studentnotes/SubjectDC.java
@@ -1,8 +1,12 @@
 package com.jkucharski.studentnotes;
 
+import android.media.Image;
+import android.widget.ImageView;
+
 public class SubjectDC {
     String name;
     String description;
+
 
     public SubjectDC(String name, String description) {
         super();
