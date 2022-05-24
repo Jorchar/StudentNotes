408326bf754b0b3dcce5c86fdbb23a29a4703181
root <vixier61@gmail.com>
2022-05-24 19:40:07 +0200

    OCR IS FINALLY WORKING

--- app/src/main/java/com/jkucharski/studentnotes/TextRecognitionFragment.java
+++ app/src/main/java/com/jkucharski/studentnotes/TextRecognitionFragment.java
@@ -5,39 +5,33 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.functions.FirebaseFunctions;
-import com.google.firebase.functions.HttpsCallableResult;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.jkucharski.studentnotes.databinding.FragmentTextRecognitionBinding;
-
-import java.io.ByteArrayOutputStream;
 
 public class TextRecognitionFragment extends Fragment {
 
     FragmentTextRecognitionBinding binding;
     FragmentManager fm;
     Bitmap imageBitmap;
-    FirebaseFunctions mFunctions;
-    String base64encoded;
+    TextRecognizer recognizer;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public TextRecognitionFragment(FragmentManager fm) {
@@ -82,58 +76,29 @@ public class TextRecognitionFragment extends Fragment {
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-            byte[] imageBytes = byteArrayOutputStream.toByteArray();
-            base64encoded = Base64.encodeToString(imageBytes, Base64.NO_WRAP);
 
             binding.imageView.setImageBitmap(imageBitmap);
         }
     }
 
     private void detectTextFromImage() {
-        mFunctions = FirebaseFunctions.getInstance();
-        JsonObject request = new JsonObject();
-        JsonObject image = new JsonObject();
-        image.add("content", new JsonPrimitive(base64encoded));
-        request.add("image", image);
-        JsonObject feature = new JsonObject();
-        feature.add("type", new JsonPrimitive("TEXT_DETECTION"));
-        JsonArray features = new JsonArray();
-        features.add(feature);
-        request.add("features", features);
+        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        InputImage image = InputImage.fromBitmap(imageBitmap, 0);
 
-        annotateImage(request.toString())
-                .addOnCompleteListener(new OnCompleteListener<JsonElement>() {
-                    @Override
-                    public void onComplete(@NonNull Task<JsonElement> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(getContext(), "Error with OCR text recognition"+task.getException(), Toast.LENGTH_SHORT).show();
-
-                        } else {
-                            JsonObject annotation = task.getResult().getAsJsonArray().get(0).getAsJsonObject().get("fullTextAnnotation").getAsJsonObject();
-                            System.out.format("%nComplete annotation:%n");
-                            System.out.format("%s%n", annotation.get("text").getAsString());
-                        }
-                    }
-                });
-
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                binding.covertedTextDisplay.setText(visionText.getText());
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getContext(), "FUCK", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
     }
-
-    private Task<JsonElement> annotateImage(String requestJson) {
-        return mFunctions
-                .getHttpsCallable("annotateImage")
-                .call(requestJson)
-                .continueWith(new Continuation<HttpsCallableResult, JsonElement>() {
-                    @Override
-                    public JsonElement then(@NonNull Task<HttpsCallableResult> task) {
-                        // This continuation runs on either success or failure, but if the task
-                        // has failed then getResult() will throw an Exception which will be
-                        // propagated down.
-                        return JsonParser.parseString(new Gson().toJson(task.getResult().getData()));
-                    }
-                });
-    }
-
-
 }
